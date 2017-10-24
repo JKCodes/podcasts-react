ce92a23715fd9c73ae0becb495f50ab04e8b5d09
Joe Kim <josephkim0224@gmail.com>
2017-10-24 15:34:36 -0400

    finish setting up itunes search fetch

--- routes/search.js
+++ routes/search.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var superagent = require('superagent')
 
 router.get('/:term', function(req, res, next) {
 
   var term = req.params.term
+  var url = 'http://itunes.apple.com/search'
 
-  res.json({
-    confirmation: 'success',
-    term: term
-  });
+  superagent
+  .get(url)
+  .query({media: 'podcast', term: term})
+  .set('Accept', 'application/json')
+  .end(function(err, response) {
+    if (err) {
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+
+      return
+    }
+
+    var data = JSON.parse(response.text)
+
+    res.json({
+      confirmation: 'success',
+      results: data.results
+    })
+  })
+
+
 
 });
 
